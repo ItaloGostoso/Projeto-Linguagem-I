0ee404bd7260db83220507936c252c0f3dfb5049
ItaloGostoso <89165297+ItaloGostoso@users.noreply.github.com>
2021-11-30 23:06:30 -0300

    Add files via upload

---
+++ Cantina do Seu mineiro/Aluno.java
@@ -0,0 +1,16 @@
+package cantina;
+
+ public class Aluno extends Cliente {
+   private int matricula;
+   private String turma;
+   
+   
+   }
+   public int getMatricula() {
+     return matricula;
+   }
+   public void setMatricula(int matricula) {
+     this.matricula = matricula;
+   }
+	}
+}
