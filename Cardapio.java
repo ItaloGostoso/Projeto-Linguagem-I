da001c0182aa5573c5e017903e052e5907f62208
ItaloGostoso <89165297+ItaloGostoso@users.noreply.github.com>
2021-11-30 22:49:10 -0300

    Add files via upload

---
+++ Cardapio.java
@@ -0,0 +1,40 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+System.out.println("\n\nCardapio do dia(Segunda feira)"); 
+System.out.println("\n____________________________________"); 
+System.out.println("\n\nPrato do dia|Acompanhamento");
+System.out.println("\n____________________________________"); 		
+System.out.println("\nCarne assada|arroz, feijão e salada");		
+System.out.println("____________________________________\n"); 
+System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+	System.out.println("\n\nCardapio do dia(Terça feira)"); 
+System.out.println("\n____________________________________"); 
+System.out.println("\nPrato do dia|Acompanhamento");
+System.out.println("\n____________________________________"); 		
+System.out.println("Lasanha|arroz, feijão e salada");		
+System.out.println("____________________________________\n");
+System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+	System.out.println("\n\nCardapio do dia(Quarta feira)"); 
+System.out.println("\n____________________________________"); 
+System.out.println("\nPrato do dia|Acompanhamento");
+System.out.println("\n____________________________________"); 		
+System.out.println("Feijoada|arroz, e salada");		
+System.out.println("____________________________________\n"); 
+System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+	System.out.println("\n\nCardapio do dia(Quinta feira)"); 
+System.out.println("\n____________________________________"); 
+System.out.println("\nPrato do dia|Acompanhamento");
+System.out.println("\n____________________________________");		
+System.out.println("\nCarne Assada|arroz, e salada");		
+System.out.println("____________________________________\n"); 
+System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+	System.out.println("\n\nCardapio do dia(Sexta feira)"); 
+System.out.println("\n____________________________________"); 
+System.out.println("\nPrato do dia|Acompanhamento");
+System.out.println("\n____________________________________");		
+System.out.println("\nPanqueca|arroz, feijão e salada");		
+System.out.println("____________________________________\n"); 
+	}
+}
+
